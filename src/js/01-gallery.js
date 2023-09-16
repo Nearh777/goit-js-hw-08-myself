4647f00259f4286899181fd2ec3b112af3961f44
Nearh777 <nearh777@gmail.com>
2023-09-16 11:33:55 +0300

    finish task 01-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryList = document.querySelector(".gallery");
+
+const cardsMarkup = createImgCardMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryList.addEventListener("click", onImgClick);
+
+function createImgCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `  
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join("");
+}
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  const isImgCardEl = evt.target.classList.contains("gallery__image");
+
+  if (!isImgCardEl) {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  scrollZoom: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+    const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_CURRENT_TIME = 'videoplayer-current-time';
+    
+let isTimeNow = localStorage.getItem(KEY_CURRENT_TIME)
+
+
+
+function CurrentTime(e) {
+    localStorage.setItem(KEY_CURRENT_TIME, e.seconds);
+};
+
+function getCurrentTime(e) {
+    if(isTimeNow) {
+        player.setCurrentTime(isTimeNow);
+        player.off('play', getCurrentTime);
+    }
+    player.on('timeupdate', Throttle(CurrentTime, 1000));
+};
+
+player.on('play', getCurrentTime);
+
+
+
+//     player.on('timeupdate', function(currentTime) {
+//         console.dir(currentTime.seconds);
+//         localStorage.setItem("time", JSON.stringify(currentTime.seconds))
+//     });
+
+//     player.on('pause', function(currentTime) {
+        
+//         localStorage.getItem("time", JSON.stringify(currentTime.seconds)) ?? []
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+
+    
+    
 
